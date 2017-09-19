46c37c6ab252e6d5ecfc5b51811a34b3d76bc656
lianghaowei <liang199611@outlook.com>
2017-09-19 13:48:43 +0800

    add facerec and get person name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,97 @@
 // This sample uses the Apache HTTP client library(org.apache.httpcomponents:httpclient:4.2.4)
 // and the org.json library (org.json:json:20170516).
 
-import java.io.File;
-import java.net.URI;
-import java.nio.file.Files;
-import java.util.stream.IntStream;
-
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-public class Main
-{
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-    // Replace the subscriptionKey string value with your valid subscription key.
+public class Main {
     public static final String subscriptionKey = "3462757bd1bb44f2a84c8cd7f1688e1a";
-
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
     public static final String uriBase = "https://api.cognitive.azure.cn/face/v1.0/detect";
+    public static final String personGroupId = "8b83ebf4-cfbb-4710-9998-14007cf577fd";
 
+    public static void getFaceName(List<String> faceIds) {
+        final String uriBase = "https://api.cognitive.azure.cn/face/v1.0/identify";
+        try {
 
-    public static void main(String[] args)
-    {
-        HttpClient httpclient = new DefaultHttpClient();
-        long startTime=System.currentTimeMillis();   //获取开始时间
-
-        try
-        {
             URIBuilder builder = new URIBuilder(uriBase);
 
-            // Request parameters. All of them are optional.
+            Map<String, Object> map = new HashMap<>();
+            map.put("faceIds", faceIds);
+            map.put("personGroupId", personGroupId);
+
+            System.out.println(JSONObject.toJSONString(map));
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            StringEntity stringEntity = new StringEntity(JSONObject.toJSONString(map));
+            request.setEntity(stringEntity);
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                String jsonString = EntityUtils.toString(entity).trim();
+                List<String> res = JSONObject.parseArray(jsonString, JSONObject.class).stream()
+                        .map(jsonObject -> jsonObject.getJSONArray("candidates"))
+                        .map(jsonArray -> jsonArray.getJSONObject(0))
+                        .map(jsonObject -> jsonObject.getString("personId"))
+                        .collect(Collectors.toList());
+                res.parallelStream().map(Main::getNameById).forEach(System.out::println);
+            }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getNameById(String personId) {
+        final String url = String.format("https://api.cognitive.azure.cn/face/v1.0/persongroups/%s/persons/%s", personGroupId, personId);
+        try {
+            URIBuilder builder = new URIBuilder(url);
+            URI uri = builder.build();
+            HttpGet request = new HttpGet(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                return JSONObject.parseObject(EntityUtils.toString(entity).trim()).getString("name");
+            }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HttpClient httpclient = new DefaultHttpClient();
+        for(int i =0;i<10;i++)
+        try {
+            long startTime = System.currentTimeMillis();   //获取开始时间
+            URIBuilder builder = new URIBuilder(uriBase);
             builder.setParameter("returnFaceId", "true");
             builder.setParameter("returnFaceLandmarks", "false");
             builder.setParameter("returnFaceAttributes", "");
@@ -60,43 +104,39 @@ public class Main
             request.setHeader("Content-Type", "application/octet-stream");
             //request.setHeader("Content-Type", "application/json");
             request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            File file = new File("haowei.jpg");
+
+            File file;
+            if (i%2 ==0)
+                file = new File("dad.png");
+            else
+                file = new File("family.png");
             byte[] picBytes = Files.readAllBytes(file.toPath());
 
             // Request body.
-            StringEntity reqEntity = new StringEntity("{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg\"}");
             ByteArrayEntity byteArrayEntity = new ByteArrayEntity(picBytes);
             request.setEntity(byteArrayEntity);
 
 
-            for(int i =0;i<1;i++) {
-                // Execute the REST API call and get the response entity.
-                HttpResponse response = httpclient.execute(request);
-                HttpEntity entity = response.getEntity();
+            // Execute the REST API call and get the response entity.
+            HttpResponse response = httpclient.execute(request);
+            long endTime = System.currentTimeMillis(); //获取结束时间
+            System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+            HttpEntity entity = response.getEntity();
 
-                if (entity != null) {
-                    // Format and display the JSON response.
-                    System.out.println("REST Response:\n");
-
-                    String jsonString = EntityUtils.toString(entity).trim();
-                    if (jsonString.charAt(0) == '[') {
-                        JSONArray jsonArray = new JSONArray(jsonString);
-                        System.out.println(jsonArray.toString(2));
-                    } else if (jsonString.charAt(0) == '{') {
-                        JSONObject jsonObject = new JSONObject(jsonString);
-                        System.out.println(jsonObject.toString(2));
-                    } else {
-                        System.out.println(jsonString);
-                    }
-                }
+            if (entity != null) {
+                String jsonString = EntityUtils.toString(entity).trim();
+                System.out.println(jsonString);
+                List<String> res = JSONObject.parseArray(jsonString).stream()
+                        .map(object -> (JSONObject) object)
+                        .map(jsonObject -> jsonObject.getString("faceId"))
+                        .collect(Collectors.toList());
+                getFaceName(res);
+                long endTime2 = System.currentTimeMillis(); //获取结束时间
+                System.out.println("程序运行时间： " + (endTime2 - endTime) + "ms");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             // Display error message.
-            System.out.println(e.getMessage());
+            System.out.println("error" + e.getMessage());
         }
-        long endTime=System.currentTimeMillis(); //获取结束时间
-        System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
     }
 }
